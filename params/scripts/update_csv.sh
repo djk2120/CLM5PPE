f7df9334101ba54d5f382c65ed97e47a61b5ebbd
Daniel Kennedy <djk2120@columbia.edu>
2020-04-13 06:38:53 -0600

    move curl into python script

--- params/scripts/update_csv.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-# downloads latest version of sheet1 from the params google drive
-curl https://docs.google.com/spreadsheets/d/e/2PACX-1vQs413GtLXtHVDCqEPgAwn4BbDjoWmV7uFqOAWH4mgpxXoVfN6ijnJdhyRgLkV-n2eU-sSQush4CzYU/pub?output=csv > params.csv
