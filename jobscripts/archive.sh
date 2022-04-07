aae51cb82cd013d441aec419d2d60f67d696c601
Daniel Kennedy <djk2120@columbia.edu>
2022-04-07 12:14:52 -0600

    cleaning up

--- jobscripts/archive.sh
+++ jobscripts/archive.sh
@@ -7,15 +7,21 @@ then
     exit 1
 fi
 
-
+#place to track collisions
 :>collisions.txt
 
 #set up environment variables
 source $1
 moveFiles=$2
 
-nc=${#cases[@]}
+#create directories
+dirs=($HIST $SPIN $REST)
+for d in ${dirs[@]}; do
+    mkdir -p $d
+done
 
+#loop through simulations
+nc=${#cases[@]}
 while read p; do
     missing=0
     i=0
@@ -27,17 +33,17 @@ while read p; do
 	else
 	    newdir=$HIST
 	fi
-	restdir=$REST
 	
-	#find restart from existing case
+	#find clm restart from existing case
 	olddir=$SCRATCH$case"_"$p"/run/"
 	r=$(echo $olddir*".clm2.r."*)
 	
-	if [ -f $r ]; then  
-	    #restart exists
+	if [ -f $r ]; then  	    #restart exists
+	    #archive restart
 	    f=$(basename $r)
-	    bash ./move.sh $f $olddir $restdir $moveFiles
+	    bash ./move.sh $f $olddir $REST $moveFiles
 	    
+	    #archive all hfiles
 	    h=$(echo $olddir*".h*.*")
 	    htapes=($(ls $h))
 	    for htape in ${htapes[@]};do
@@ -55,5 +61,16 @@ while read p; do
 	echo $case"_"$p" generates restarts"
     fi
 done<$paramList
-    
-    
+
+
+collisions=$(wc -l < collisions.txt)
+if [[ $collisions > 0 ]]; then
+    echo $collisions" files already existed in the archive"
+    if [[ $moveFiles -lt 2 ]]; then
+	echo "Those files were NOT overwritten"
+    else
+	echo "Those files were overwritten"
+    fi
+    echo "see collisions.txt for file list"
+fi
+
